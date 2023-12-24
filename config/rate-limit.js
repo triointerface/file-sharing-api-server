856af69d104defac46ce10d1774a62c827269a3d
Jobayer Ahmed <Jobayer6735@gmail.com>
2023-12-24 21:49:28 +0600

    Modfieid the daily upload & download limit error message

--- config/rate-limit.js
+++ config/rate-limit.js
@@ -4,17 +4,17 @@ import { maxUploadRequests, maxDownloadRequests } from './env.js';
 export const uploadLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours
   max: maxUploadRequests, // Adjust the maximum number of requests
-  statusCode: 200,
+  statusCode: 429,
   message: {
-    status: 429,
-    limiter: true,
-    type: "error",
-    errpr: 'maximum_accounts'
+    error: 'Daily upload limit exceeded. Please try again tomorrow.'
   }
 });
 
 export const downloadLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours
   max: maxDownloadRequests, // Adjust the maximum number of requests
-  message: 'Too many download requests from this IP, please try again later.',
+  statusCode: 429,
+  message: {
+    error: 'Daily download limit exceeded. Please try again tomorrow.'
+  },
 });
